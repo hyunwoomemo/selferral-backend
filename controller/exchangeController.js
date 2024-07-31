a91227838f1b471a10af375826a84ec247cf9969
leehyunwoo <archihw94@gmail.com>
2024-07-31 23:04:46 +0900

    edit

--- controller/exchangeController.js
+++ controller/exchangeController.js
@@ -3,15 +3,11 @@ const exchangeModel = require("../model/exchangeModel");
 require("dotenv").config();
 
 exports.getAll = async (req, res) => {
-  console.log("req", req);
-
   const exchanges = await exchangeModel.getAllExchanges();
 
   res.status(200).json({ data: exchanges });
 };
 
 exports.test = async (req, res) => {
-  console.log("req res", req, res);
-
   res.status(200).json("test");
 };
